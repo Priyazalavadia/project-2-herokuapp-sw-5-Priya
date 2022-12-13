0cb166e3aaaca95e52436ab1d0823fc73d0881c9
Priyazalavadia <priyazalavadia@gmail.com>
2022-12-13 22:40:19 +0000

    project-2-herokuapp-sw-5-Priya

---
+++ src/main/java/com/herokuapp/pages/LoginPage.java
@@ -0,0 +1,62 @@
+package com.herokuapp.pages;
+
+import com.aventstack.extentreports.Status;
+import com.herokuapp.customlisteners.CustomListeners;
+import com.herokuapp.utility.Utility;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends Utility {
+
+    @CacheLookup
+    @FindBy(id = "username")
+    WebElement enterUsername;
+
+    @CacheLookup
+    @FindBy(id = "password")
+    WebElement enterPassword;
+    @CacheLookup
+    @FindBy(xpath = "//button[@class='radius']")
+    WebElement clickLogin;
+    @CacheLookup
+    @FindBy(xpath = "//div[@class= 'flash error']")
+    WebElement passwordErrorMessage;
+    @CacheLookup
+    @FindBy(xpath = "//div[@id='flash']")
+            WebElement usernameInvalidMessage;
+    @CacheLookup
+    @FindBy(xpath = "//h2[normalize-space()='Secure Area']")
+            WebElement secureAreaText;
+
+
+    public void usernameField(String email){
+        CustomListeners.test.log(Status.PASS,"Enter EmailId" + email);
+        sendTextToElement(enterUsername,email);
+    }
+    public void passwordField(String password){
+        CustomListeners.test.log(Status.PASS,"Enter Passcode "+ password);
+        sendTextToElement(enterPassword,password);
+    }
+
+    public void clickLoginButton(){
+        CustomListeners.test.log(Status.PASS, "Click on loginButton");
+        clickOnElement(clickLogin);
+    }
+
+    public String passwordError(){
+        CustomListeners.test.log(Status.PASS, "PasswordError");
+        return getTextFromElement(passwordErrorMessage);
+    }
+
+    public String usernameError(){
+        CustomListeners.test.log(Status.PASS,"Username Error");
+        return getTextFromElement(usernameInvalidMessage);
+    }
+
+    public String getSecureAreaMessage(){
+        CustomListeners.test.log(Status.PASS, "Get Secure Area Message");
+        return getTextFromElement(secureAreaText);
+    }
+}
